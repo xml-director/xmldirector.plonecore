d06b362804bdc479e4baaafdddbede3ee2952dff
zopyx <yet@gmx.de>
2014-05-22 14:17:55 +0200

    removed

--- zopyx/existdb/browser/compatible.py
+++
@@ -1,9 +0,0 @@
-# zopyx.existdb
-# (C) 2013,  ZOPYX Limited, D-72074 Tuebingen, Germany
-################################################################
-
-try:
-    from App.class_init import InitializeClass
-except ImportError:
-    from Globals import InitializeClass
-
