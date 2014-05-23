fa1e77eebc3a676dace2f34ecb74cccf4750e4d5
zopyx <yet@gmx.de>
2014-05-23 08:40:28 +0200

    updated

--- zopyx/existdb/browser/connector.py
+++ zopyx/existdb/browser/connector.py
@@ -1,12 +1,39 @@
-import fs
 import urllib
 import mimetypes
+import fs
+import fs.errors
+import zExceptions
 from fs.opener import opener
 from fs.contrib.davfs import DAVFS
 from Products.Five.browser import BrowserView
+from zope.interface import implements
 from zope.interface import implementer
 from zope.publisher.interfaces import IPublishTraverse
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+
+from ZPublisher.Iterators import IStreamIterator
+
+class webdav_iterator(file):
+
+    implements(IStreamIterator)
+
+    def __init__(self, handle, mode='rb', streamsize=1<<16):
+        self.fp = handle.open('.', mode)
+        self.streamsize = streamsize
+
+    def next(self):
+        data = self.fp.read(self.streamsize)
+        if not data:
+            raise StopIteration
+        return data
+
+    def __len__(self):
+        cur_pos = self.fp.tell()
+        self.fp.seek(0, 2)
+        size = self.fp.tell()
+        self.seek(cur_pos, 0)
+        return size
 
 
 @implementer(IPublishTraverse)
@@ -21,7 +48,11 @@ class Connector(BrowserView):
 
     def __call__(self, *args, **kw):
         url = self.context.url + '/' + urllib.quote('/'.join(self.subpath))
-        handle = DAVFS(url, credentials=dict(username='admin', password='pnmaster'))
+        try:
+            handle = DAVFS(url, credentials=dict(username='admin', password='pnmaster'))
+        except fs.errors.ResourceNotFoundError:
+            raise zExceptions.NotFound()
+            
         if handle.isdir('.'):
             files = handle.listdirinfo(files_only=True)
             files = sorted(files)
@@ -38,6 +69,7 @@ class Connector(BrowserView):
             self.request.response.setHeader('Content-Type', mt)
             if 'size' in info:
                 self.request.response.setHeader('Content-Length', info['size'])
+            return webdav_iterator(handle)
             return handle.open('.', 'rb').read()
         else:
             raise RuntimeError()
