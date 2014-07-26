6b9484c164ae1d8f1766fd2295aa66e8bc7beff4
zopyx <yet@gmx.de>
2014-07-26 08:35:11 +0200

    updated

---
+++ zopyx/existdb/tests/base.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+################################################################
+# zopyx.existdb
+# (C) 2014,  Andreas Jung, www.zopyx.com, Tuebingen, Germany
+################################################################
+
+import unittest2
+from plone.app.testing import PloneSandboxLayer
+from plone.app.testing import applyProfile
+from plone.app.testing import PLONE_FIXTURE
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import setRoles
+from plone.app.testing import login
+from plone.testing import z2
+
+from zope.configuration import xmlconfig
+from AccessControl.SecurityManagement import newSecurityManager
+
+import zopyx.existdb
+import plone.app.dexterity
+
+class PolicyFixture(PloneSandboxLayer):
+
+    defaultBases = (PLONE_FIXTURE,)
+
+    def setUpZope(self, app, configurationContext):
+
+#        xmlconfig.file('meta.zcml', z3c.jbot, context=configurationContext)
+        for mod in [plone.app.dexterity,
+                    zopyx.existdb,
+                    ]:
+            xmlconfig.file('configure.zcml', mod, context=configurationContext)
+
+        # Install product and call its initialize() function
+#        z2.installProduct(app, 'eteaching.policy')
+
+    def setUpPloneSite(self, portal):
+        # Install into Plone site using portal_setup
+        applyProfile(portal, 'zopyx.existdb:default')
+        portal.acl_users.userFolderAddUser('god', 'dummy', ['Manager'], [])
+        setRoles(portal, 'god', ['Manager'])
+        portal.acl_users.userFolderAddUser('ppr', 'dummy', ['PPR'], [])
+        setRoles(portal, 'ppr', ['Member', 'PPR'])
+        portal.acl_users.userFolderAddUser('member', 'dummy', ['Member'], [])
+        setRoles(portal, 'member', ['Member'])
+        login(portal, 'god')
+
+    def tearDownZope(self, app):
+        # Uninstall product
+        z2.uninstallProduct(app, 'zopyx.existdb')
+
+
+POLICY_FIXTURE = PolicyFixture()
+POLICY_INTEGRATION_TESTING = IntegrationTesting(
+    bases=(POLICY_FIXTURE,), name='PolicyFixture:Integration')
+
+
+class TestBase(unittest2.TestCase):
+
+    layer = POLICY_INTEGRATION_TESTING
+
+    @property
+    def portal(self):
+        return self.layer['portal']
+
+    def login(self, uid='god'):
+        """ Login as manager """
+        user = self.portal.acl_users.getUser(uid)
+        newSecurityManager(None, user.__of__(self.portal.acl_users))
---
+++ zopyx/existdb/tests/test_connector.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+################################################################
+# zopyx.existdb
+# (C) 2014,  Andreas Jung, www.zopyx.com, Tuebingen, Germany
+################################################################
+
+import os
+from base import TestBase
+import plone.api
+
+
+class BasicTests(TestBase):
+
+    def testCreateTestFolder(self):
+        self.login('god')
+        import pdb; pdb.set_trace() 
+        connector = plone.api.content.create(self.portal, 'zopyx.existdb.connector', id='connector')
+        assert connector.portal_type == 'zopyx.existdb.connector'
+
+
+def test_suite():
+    from unittest2 import TestSuite, makeSuite
+    suite = TestSuite()
+    suite.addTest(makeSuite(BasicTests))
+    return suite
