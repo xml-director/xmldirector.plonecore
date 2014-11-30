354780c8f1ed11616dc753229db6dd1f863d6249
zopyx <yet@gmx.de>
2014-11-30 10:35:47 +0100

    updated

--- zopyx/existdb/browser/resources/local.js
+++ zopyx/existdb/browser/resources/local.js
@@ -1,2 +1,21 @@
 $(document).ready(function() {
+
+    $('.template-edit .xml-field').each(function() {
+
+        $(this).hide();
+
+        var id = $(this).attr('id');
+        $(this).after('<div id="' + id + '-chars"></div>');
+        $(this).after('<div id="' + id + '-editor" style="width: 80%; min-height: 200px; height: 400px; max-height: 400px">hello</div>')
+        var editor = ace.edit(id + '-editor');
+        editor.setTheme("ace/theme/chrome");
+        editor.getSession().setMode("ace/mode/xml");
+        editor.setShowPrintMargin(false);
+        editor.getSession().setValue($(this).val());
+        editor.getSession().on('change', function(){
+              var xml = editor.getSession().getValue();
+              $('#' + id).val(xml)
+              $('#' + id + '-chars').text(xml.length + ' chars');
+        });
+    });
 });
--- zopyx/existdb/dx/fields.py
+++ zopyx/existdb/dx/fields.py
@@ -4,16 +4,24 @@
 ################################################################
 
 
+import lxml.etree
 from zope.schema import Text
 from zopyx.existdb.i18n import MessageFactory as _
 
 from plone.schemaeditor.interfaces import IFieldFactory
 from plone.schemaeditor.fields import FieldFactory
+import plone.supermodel.exportimport
 
 
 class XML(Text):
-    pass
+
+    def validate(self, value):
+
+        try:
+            root = lxml.etree.fromstring(value)
+        except lxml.etree.XMLSyntaxError as e:
+            raise ValueError(u'XML syntax error {}'.format(e))
 
 
 XMLFactory = FieldFactory(XML, _(u'label_xml_field', default=u'XML'))
-
+XMLHandler = plone.supermodel.exportimport.BaseHandler(XML)
