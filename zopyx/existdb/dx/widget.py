5bac13d2357883847ce52073e14dd32df793b57f
zopyx <yet@gmx.de>
2014-12-05 13:26:19 +0100

    updated

--- zopyx/existdb/dx/widget.py
+++ zopyx/existdb/dx/widget.py
@@ -3,6 +3,7 @@
 # (C) 2014,  Andreas Jung, www.zopyx.com, Tuebingen, Germany
 ################################################################
 
+import lxml
 
 import zope.component
 import zope.interface
@@ -24,6 +25,7 @@ from z3c.form import converter
 from z3c.form.browser import text
 from zope.interface import implementsOnly, implementer
 from zope.component import adapter
+from zope.component import getUtility
 
 
 from z3c.form.interfaces import IWidget
@@ -38,8 +40,37 @@ class XPathWidget(text.TextWidget):
     zope.interface.implementsOnly(IXPathWidget)
 
     def xpath_to_value(self):
+
+        from plone.dexterity.interfaces import IDexterityFTI
+        from plone.behavior.interfaces import IBehaviorAssignable
+         
+        schema = zope.component.getUtility(
+            IDexterityFTI, name=self.context.portal_type).lookupSchema()
+        fields = dict((fieldname, schema[fieldname]) for fieldname in schema)
+
+        assignable = IBehaviorAssignable(self.context)
+        for behavior in assignable.enumerateBehaviors():
+            behavior_schema = behavior.interface
+            fields.update((name, behavior_schema[name]) for name in behavior_schema)
+
         print self.value
-        return 'foo'
+        parts = self.value.split(',', 1)
+        fieldname = parts[0].split('=')[1]
+        xpath_expr = parts[1].split('=')[1]
+        print fieldname
+        print xpath_expr
+        xml_field = fields.get(fieldname)
+        if not xml_field: 
+            raise ValueError('No such field "{}"'.format(fieldname))
+
+        import pdb; pdb.set_trace()                         
+
+        from zopyx.existdb.dx.fields import AttributeField
+        adapter = AttributeField(context=self.context, field=xml_field)
+        xml = adapter.get()
+        root = lxml.etree.fromstring(xml)
+        result = root.xpath(xpath_expr)
+        return result
 
 
 class XPathDataConverter(converter.FieldDataConverter):
