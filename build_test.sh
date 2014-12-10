3a3b1bed4623253942d52a5ab691a689bc182542
zopyx <yet@gmx.de>
2014-12-10 08:45:17 +0100

    fix^

--- build_test.sh
+++ build_test.sh
@@ -16,7 +16,7 @@ fi
 
 export WEBDAV_URL=http://dev1.veit-schiele.de:6080
 
-virtualenv-2.7
+virtualenv-2.7 .
 bin/pip install -U setuptools
 bin/python bootstrap.py -c $config
 bin/buildout -c $config
