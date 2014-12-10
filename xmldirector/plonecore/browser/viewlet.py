425465b3d440a93e2867c34fc1c76de4f463bcc4
zopyx <yet@gmx.de>
2014-12-10 16:53:26 +0100

    added

---
+++ xmldirector/plonecore/browser/viewlet.py
@@ -0,0 +1,17 @@
+################################################################
+# xmldirector.plonecore
+# (C) 2014,  Andreas Jung, www.zopyx.com, Tuebingen, Germany
+################################################################
+
+
+from plone.app.layout.viewlets import ViewletBase
+from xmldirector.plonecore.dx import util
+
+
+class Debug(ViewletBase):
+
+    def info(self):
+        if not util.is_xml_content(self.context):
+            return
+        return dict(storage_key=util.get_storage_key(self.context),
+                    storage_path=util.get_storage_path(self.context))
