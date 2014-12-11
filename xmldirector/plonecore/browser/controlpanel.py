08201a89499a572b832aac319ac7ad9c2b3110bb
zopyx <yet@gmx.de>
2014-12-11 14:34:40 +0100

    connection test

--- xmldirector/plonecore/browser/controlpanel.py
+++ xmldirector/plonecore/browser/controlpanel.py
@@ -5,10 +5,14 @@
 # (C) 2014,  Andreas Jung, www.zopyx.com, Tuebingen, Germany
 ################################################################
 
+
+import fs.errors
+from zope.component import getUtility
 from plone.app.registry.browser import controlpanel
 
-from xmldirector.plonecore.interfaces import IWebdavSettings
 from xmldirector.plonecore.i18n import MessageFactory as _
+from xmldirector.plonecore.interfaces import IWebdavSettings
+from xmldirector.plonecore.interfaces import IWebdavHandle
 
 
 class DBSettingsEditForm(controlpanel.RegistryEditForm):
@@ -26,3 +30,22 @@ class DBSettingsEditForm(controlpanel.RegistryEditForm):
 
 class DBSettingsControlPanel(controlpanel.ControlPanelFormWrapper):
     form = DBSettingsEditForm
+
+    def connection_test(self):
+
+        service = getUtility(IWebdavHandle)
+        errors = []
+
+        try:
+            handle = service.webdav_handle()
+        except fs.errors.PermissionDeniedError as e:
+            errors.append(u'Permission denied error - improper credentials? ({})'.format(e))
+            return errors
+        except fs.errors.ResourceNotFoundError as e:
+            errors.append(u'Resource not found - local webdav path correct? ({})'.format(e))
+            return errors
+        except fs.errors.RemoteConnectionError as e:
+            errors.append(u'WebDAV URL incorrect! ({})'.format(str(e)))
+            return errors
+
+        return errors
