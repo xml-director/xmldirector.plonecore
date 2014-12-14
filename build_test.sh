8ff7f30ff188b968b746af210d99939489fc588b
zopyx <yet@gmx.de>
2014-12-14 11:12:08 +0100

    updated

--- build_test.sh
+++ build_test.sh
@@ -17,7 +17,7 @@ fi
 export WEBDAV_URL=http://dev1.veit-schiele.de:6080/exist/webdav/db
 
 virtualenv-2.7 .
-bin/pip install -U setuptools
+bin/pip install -U setuptools==7.0 
 bin/python bootstrap.py -c $config
 bin/buildout -c $config
 bin/test -s xmldirector
