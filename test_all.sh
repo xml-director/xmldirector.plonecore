0ab0cb9f5dd45a48589254a4764cdd69f7d73c60
Andreas Jung <yet@gmx.de>
2014-12-29 18:38:34 +0100

    updated

--- test_all.sh
+++ test_all.sh
@@ -1,5 +1,7 @@
+#EXISTDB
 export WEBDAV_URL=http://dev1.veit-schiele.de:22081/exist/webdav/db
 bin/test xmldirector
 
-export WEBDAV_URL=http://dev1.veit-schiele.de:22081/webdav
+#BASEX
+export WEBDAV_URL=http://dev1.veit-schiele.de:22080/webdav
 bin/test xmldirector
