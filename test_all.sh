675e311cfd93d8d4d3d42a3a4536150711e4d0fa
zopyx <yet@gmx.de>
2014-12-30 10:55:01 +0100

    updated

--- test_all.sh
+++ test_all.sh
@@ -1,4 +1,8 @@
-#EXISTDB
+# Test xmldirector.plonecore against all supported database backend
+# The specified webdav endpoint are based on Docker containers
+
+export WEBDAV_USERNAME=admin
+export WEBDAV_PASSWORD=admin
 
 export WEBDAV_URL=http://dev1.veit-schiele.de:22081/exist/webdav/db
 echo '######################################################'
