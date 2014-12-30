f0d4547d6f94377bc380bc3b8ae8aeba0d85bb06
zopyx <yet@gmx.de>
2014-12-30 10:46:43 +0100

    updated

--- test_all.sh
+++ test_all.sh
@@ -1,7 +1,15 @@
 #EXISTDB
+
 export WEBDAV_URL=http://dev1.veit-schiele.de:22081/exist/webdav/db
+echo '######################################################'
+echo Tests against EXISTDB
+echo $WEBDAV_URL
+echo '######################################################'
 bin/test xmldirector
 
-#BASEX
 export WEBDAV_URL=http://dev1.veit-schiele.de:22080/webdav
+echo '######################################################'
+echo Tests against BASEX
+echo $WEBDAV_URL
+echo '######################################################'
 bin/test xmldirector
