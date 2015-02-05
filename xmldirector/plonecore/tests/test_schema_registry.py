9117bd374797c47fb080178a07bd5ffb725f53a6
zopyx <yet@gmx.de>
2015-02-05 13:16:25 +0100

    added tests for schema/dtd registry

---
+++ xmldirector/plonecore/tests/test_schema_registry.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+################################################################
+# xmldirector.plonecore
+# (C) 2014,  Andreas Jung, www.zopyx.com, Tuebingen, Germany
+################################################################
+
+import os
+import unittest2
+from xmldirector.plonecore.schema_registry import SchemaRegistry
+
+cwd = os.path.dirname(__file__)
+
+
+class BasicTests(unittest2.TestCase):
+
+    def setUp(self):
+        self.registry = SchemaRegistry()
+        self.registry.clear()
+        self.registry.parse_folder(
+                family='testing',
+                directory=os.path.join(cwd, 'schemas'))
+
+    def test_parsing_improper_directory(self):
+        with self.assertRaises(IOError):
+            self.registry.parse_folder(
+                    family='testing',
+                    directory='does.not.exist')
+
+    def test_registry(self):
+        self.assertEquals(len(self.registry), 1)
+        self.assertEqual('testing::DGHO_Webcontent_20150202.dtd' in self.registry.schema_registry.keys(), True) 
+
+    def test_get_schema(self):
+        validator = self.registry.get_schema('testing', 'DGHO_Webcontent_20150202.dtd')
+        with self.assertRaises(ValueError):
+            validator = self.registry.get_schema('testing', 'does.not.exist')
+
+
+
+def test_suite():
+    from unittest2 import TestSuite, makeSuite
+    suite = TestSuite()
+    suite.addTest(makeSuite(BasicTests))
+    return suite
