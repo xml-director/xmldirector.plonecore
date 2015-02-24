e753881d76e94b606a72cd1cc4cc52eaf03cd8a7
zopyx <yet@gmx.de>
2015-02-24 16:15:45 +0100

    wrapper for checking locks

--- xmldirector/plonecore/davfs.py
+++ xmldirector/plonecore/davfs.py
@@ -7,10 +7,17 @@
 from fs.contrib.davfs import DAVFS
 from fs import iotools
 
+def check_locks(method):
+    """ Decorator to register a method as a transformation"""
+    def wrapper(*args, **kw):
+        return method(*args, **kw)
+    return wrapper
+
 
 class DAVFSWrapper(DAVFS):
     """ A wapper for DAVFS """
 
     @iotools.filelike_to_stream
+    @check_locks
     def open(self,path,mode="r", **kwargs):
         return super(DAVFSWrapper, self).open(path, mode, **kwargs)
