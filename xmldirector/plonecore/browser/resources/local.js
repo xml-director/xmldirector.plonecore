36055a64cddba11fd340a8604a17e2e4dafa1e06
zopyx <yet@gmx.de>
2015-08-23 18:35:23 +0200

    call DataTable init code only if there are datatables

--- xmldirector/plonecore/browser/resources/local.js
+++ xmldirector/plonecore/browser/resources/local.js
@@ -178,65 +178,67 @@ $(document).ready(function() {
 
     var order = [0, "asc"];
 
-    $('.datatable tfoot th.searchable').each(function () {
-        if ($(this).children().length == 0) {
-            var title = $('.datatable thead th').eq( $(this).index() ).text();
-            $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
-        }
-    });
-
-    var table = $('.datatable').DataTable({
-
-        columnDefs: [ {
-          "targets": 'no-sort',
-                     "orderable": false,
-        } ],
-        pageLength: 50,
-        autoWidth: false,
-        initComplete: function(settings, json) {
-            $('.datatable').show();
-        },
-        order: order,
-        aLengthMenu: [25, 50, 100, 250, 500, 750, 1000, 2000, 3000],
-        // dom: 'TC<"clear">lfrtip',
-        dom: 'C<"clear">lfrtip',
-           tableTools: {
-        "sSwfPath": "++resource++zchl.policy/DataTables/extensions/TableTools/swf/copy_csv_xls_pdf.swf"
-        },
-        language: {
-            "sEmptyTable":      "Keine Daten in der Tabelle vorhanden",
-            "sInfo":            "_START_ bis _END_ von _TOTAL_ Einträgen",
-            "sInfoEmpty":       "0 bis 0 von 0 Einträgen",
-            "sInfoFiltered":    "(gefiltert von _MAX_ Einträgen)",
-            "sInfoPostFix":     "",
-            "sInfoThousands":   ".",
-            "sLengthMenu":      "_MENU_ Einträge anzeigen",
-            "sLoadingRecords":  "Wird geladen...",
-            "sProcessing":      "Bitte warten...",
-            "sSearch":          "Suchen",
-            "sZeroRecords":     "Keine Einträge vorhanden.",
-            "oPaginate": {
-                "sFirst":       "Erste",
-                "sPrevious":    "Zurück",
-                "sNext":        "Nächste",
-                "sLast":        "Letzte"
-            },
-            "oAria": {
-                "sSortAscending":  ": aktivieren, um Spalte aufsteigend zu sortieren",
-                "sSortDescending": ": aktivieren, um Spalte absteigend zu sortieren"
+    var tables = $('.datatable');
+    if (tables.length > 0) {
+        $('.datatable tfoot th.searchable').each(function () {
+            if ($(this).children().length == 0) {
+                var title = $('.datatable thead th').eq( $(this).index() ).text();
+                $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
             }
-        }
-    });
+        });
 
-    table.columns().every( function () {
-        var that = this;
-        $( 'input', this.footer() ).on( 'keyup change', function () {
-            that
-                .search( this.value )
-                .draw();
+        var table = $('.datatable').DataTable({
+
+            columnDefs: [ {
+              "targets": 'no-sort',
+                         "orderable": false,
+            } ],
+            pageLength: 50,
+            autoWidth: false,
+            initComplete: function(settings, json) {
+                $('.datatable').show();
+            },
+            order: order,
+            aLengthMenu: [25, 50, 100, 250, 500, 750, 1000, 2000, 3000],
+            // dom: 'TC<"clear">lfrtip',
+            dom: 'C<"clear">lfrtip',
+               tableTools: {
+            "sSwfPath": "++resource++zchl.policy/DataTables/extensions/TableTools/swf/copy_csv_xls_pdf.swf"
+            },
+            language: {
+                "sEmptyTable":      "Keine Daten in der Tabelle vorhanden",
+                "sInfo":            "_START_ bis _END_ von _TOTAL_ Einträgen",
+                "sInfoEmpty":       "0 bis 0 von 0 Einträgen",
+                "sInfoFiltered":    "(gefiltert von _MAX_ Einträgen)",
+                "sInfoPostFix":     "",
+                "sInfoThousands":   ".",
+                "sLengthMenu":      "_MENU_ Einträge anzeigen",
+                "sLoadingRecords":  "Wird geladen...",
+                "sProcessing":      "Bitte warten...",
+                "sSearch":          "Suchen",
+                "sZeroRecords":     "Keine Einträge vorhanden.",
+                "oPaginate": {
+                    "sFirst":       "Erste",
+                    "sPrevious":    "Zurück",
+                    "sNext":        "Nächste",
+                    "sLast":        "Letzte"
+                },
+                "oAria": {
+                    "sSortAscending":  ": aktivieren, um Spalte aufsteigend zu sortieren",
+                    "sSortDescending": ": aktivieren, um Spalte absteigend zu sortieren"
+                }
+            }
+        });
+
+        table.columns().every( function () {
+            var that = this;
+            $( 'input', this.footer() ).on( 'keyup change', function () {
+                that
+                    .search( this.value )
+                    .draw();
+            } );
         } );
-    } );
-
+    }
 });
 
 });
