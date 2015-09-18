ef4652cb4f1bc446aeddbebcd0fa35acb8da2c08
Andreas Jung <yet@gmx.de>
2015-09-18 18:00:06 +0200

    PEP8

--- xmldirector/plonecore/browser/lock.py
+++ xmldirector/plonecore/browser/lock.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+
+################################################################
+# xmldirector.plonecore
+# (C) 2014,  Andreas Jung, www.zopyx.com, Tuebingen, Germany
+################################################################
 
 
 from Products.Five.browser import BrowserView
