9deb5a7615a705ff492213bbc03fdd07efe36142
zopyx <yet@gmx.de>
2015-09-30 14:13:59 +0200

    added

---
+++ xmldirector/plonecore/fswrapper.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+################################################################
+# xmldirector.plonecore
+# (C) 2014,  Andreas Jung, www.zopyx.com, Tuebingen, Germany
+################################################################
+
+
+from furl import furl
+
+from xmldirector.plonecore.davfs import have_boto
+from xmldirector.plonecore.davfs import have_paramiko
+from xmldirector.plonecore.davfs import DAVFSWrapper
+from xmldirector.plonecore.davfs import OSFSWrapper
+from xmldirector.plonecore.davfs import FTPFSWrapper
+
+
+def get_fs_wrapper(url, credentials=None):
+
+    if not url.endswith('/'):
+        url += '/'
+    f = furl(url)
+    original_url = url 
+    if f.scheme == 'file':
+        wrapper = OSFSWrapper(url[7:], encoding='utf-8')
+    elif f.scheme == 'http':
+        wrapper = DAVFSWrapper(original_url, credentials) 
+    elif f.scheme == 'https':
+        wrapper = DAVFSWrapper(original_url, credentials) 
+    elif f.scheme == 's3':
+        if have_boto:
+            from xmldirector.plonecore.davfs import S3FSWrapper
+            wrapper = S3FSWrapper(f.host)
+        else:
+            raise ValueError('boto module is not installed')
+    elif f.scheme == 'sftp':
+
+        if have_paramiko:
+            from xmldirector.plonecore.davfs import SFTPFSWrapper
+            wrapper = SFTPFSWrapper(connection=f.host, 
+                    root_path=str(f.path),
+                    username=f.username,
+                    password=f.password)
+        else:
+            raise ValueError('paramiko module is not installed')
+
+    elif f.scheme == 'ftp':
+        wrapper = FTPFSWrapper(host=f.host, 
+                user=f.username,
+                passwd=f.password)
+    else:
+        raise ValueError('Unsupport URL schema {}'.format(original_url))
+
+    wrapper.url = url
+    return wrapper
