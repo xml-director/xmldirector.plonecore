591a0c73147a403404c0f90b1a4a010459554d57
Andreas Jung <yet@gmx.de>
2015-10-03 11:52:36 +0200

    updated

--- build_test.sh
+++ build_test.sh
@@ -11,10 +11,8 @@ config=buildout-plone-$PLONE_VERSION.cfg
 
 export WEBDAV_URL=http://demo.xml-director.info:22081/exist/webdav/db
 
-virtualenv-2.7 .
-bin/pip install -U setuptools==18.2  
-bin/python bootstrap.py -c $config --setuptools-version 18.2 --version 2.2.5
-
+pip install -U setuptools==18.2  
+python bootstrap.py -c $config --setuptools-version 18.2 --version 2.2.5
 bin/buildout -c $config
 bin/test -s xmldirector.plonecore
 ##bin/coverage run bin/test
