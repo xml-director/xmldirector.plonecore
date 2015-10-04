467302dc41eb3e45a113317f6ff35e2e3b55e6ce
Andreas Jung <yet@gmx.de>
2015-10-04 09:34:49 +0200

    updated

--- build_test.sh
+++ build_test.sh
@@ -6,8 +6,12 @@ $PATH:
 
 echo $WEBDAV_URL
 echo $PLONE_VERSION
+echo $DOCKER
 
 config=buildout-plone-$PLONE_VERSION.cfg
+
+docker pull $DOCKER
+docker run -d -p 127.0.0.1:8080:8080 $DOCKER
 
 #virtualenv-2.7 .
 pip install -U setuptools==7.0  
