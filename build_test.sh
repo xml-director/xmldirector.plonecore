6cfac86e3e4eb96bf2296d5988a752df2b8ceaf0
Andreas Jung <yet@gmx.de>
2015-10-16 17:19:24 +0300

    updated

--- build_test.sh
+++ build_test.sh
@@ -15,6 +15,7 @@ docker run -d -p 127.0.0.1:8080:8080 $DOCKER
 
 #virtualenv-2.7 .
 pip install -U setuptools==7.0  
+pip install boto
 python bootstrap.py -c $config --setuptools-version 18.4 --version 2.2.5
 bin/buildout -c $config
 bin/test -s xmldirector.plonecore
