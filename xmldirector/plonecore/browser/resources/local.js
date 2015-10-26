8d067570de59218bdd6767f5cb4107df00eaca99
zopyx <yet@gmx.de>
2015-10-26 19:13:58 +0100

    updated

--- xmldirector/plonecore/browser/resources/local.js
+++ xmldirector/plonecore/browser/resources/local.js
@@ -1,4 +1,4 @@
-function init_dt() {
+function init_datatables() {
 
     try {
         var order = DATATABLES_ORDER;
@@ -69,10 +69,49 @@ function init_dt() {
     }
 }
 
-$(document).ready(function() {
 
-    init_dt();
-    return;
+function init_controlpanel() {
+
+    // Test connection button for XML Director controlpanel
+    var button = $('.template-xmldirector-core-settings #form-buttons-save');
+    if (button.length > 0) {
+
+        $.ajax('xmldirector-settings-json', {
+            success: function(settings) {
+
+                var settings = $.parseJSON(settings); 
+
+                var button2 = button.clone();
+                var cancel_button = $('#form-buttons-cancel');
+                button2.attr('value', 'Test connection');
+                button2.attr('name', 'form-button-test-connection');
+                button2.attr('id', 'form-button-test-connection');
+                button2.attr('type', 'button');
+                button2.attr('style', 'margin-left: 1em');
+                button2.insertAfter(cancel_button);
+                button2.on('click', function() {
+                    window.location.href = '@@xmldirector-connection-test'; 
+                });
+
+                if (settings['webdav_mode'] == 'existdb') {
+                    var button3 = button.clone();
+                    button3.attr('value', 'Install Exist-DB scripts');
+                    button3.attr('name', 'form-button-install-scripts');
+                    button3.attr('id', 'form-button-install-scripts');
+                    button3.attr('type', 'button');
+                    button3.attr('style', 'margin-left: 1em');
+                    button3.insertAfter(button2);
+                    button3.on('click', function() {
+                        window.location.href = '@@xmldirector-install-scripts'; 
+                    });
+                }
+            }
+        })
+    }            
+}   
+
+
+function init_ace() {
 
     var plone5 = $('[data-bundle="plone-legacy"]').length > 0;
     if (plone5) {
@@ -198,44 +237,12 @@ $(document).ready(function() {
             return false;
         }
     });
-
-    // Test connection button for XML Director controlpanel
-    var button = $('.template-xmldirector-core-settings #form-buttons-save');
-    if (button.length > 0) {
-
-        $.ajax('xmldirector-settings-json', {
-            success: function(settings) {
-
-                var settings = $.parseJSON(settings); 
-
-                var button2 = button.clone();
-                var cancel_button = $('#form-buttons-cancel');
-                button2.attr('value', 'Test connection');
-                button2.attr('name', 'form-button-test-connection');
-                button2.attr('id', 'form-button-test-connection');
-                button2.attr('type', 'button');
-                button2.attr('style', 'margin-left: 1em');
-                button2.insertAfter(cancel_button);
-                button2.on('click', function() {
-                    window.location.href = '@@xmldirector-connection-test'; 
-                });
-
-                if (settings['webdav_mode'] == 'existdb') {
-                    var button3 = button.clone();
-                    button3.attr('value', 'Install Exist-DB scripts');
-                    button3.attr('name', 'form-button-install-scripts');
-                    button3.attr('id', 'form-button-install-scripts');
-                    button3.attr('type', 'button');
-                    button3.attr('style', 'margin-left: 1em');
-                    button3.insertAfter(button2);
-                    button3.on('click', function() {
-                        window.location.href = '@@xmldirector-install-scripts'; 
-                    });
-                }
-            }
-        })
-    }            
+}
 
 
+$(document).ready(function() {
+    init_datatables();
+    init_controlpanel();
+//    init_ace();
 });
 
