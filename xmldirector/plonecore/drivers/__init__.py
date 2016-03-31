989ce1e4b3e7c6608da930d766d5b6076b25c592
Andreas JungÂ <info@zopyx.com>
2016-03-31 08:17:55 +0200

    removed

--- xmldirector/plonecore/drivers/__init__.py
+++
@@ -1,6 +0,0 @@
-################################################################
-# xmldirector.plonecore
-# (C) 2014,  Andreas Jung, www.zopyx.com, Tuebingen, Germany
-################################################################
-
-import dropbox_opener  # NOQA
