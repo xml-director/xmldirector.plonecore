9b74bef13da59c035fd0dec1c2fba2f55d704224
Andreas JungÂ <info@zopyx.com>
2016-04-18 10:47:05 +0200

    updated

--- xmldirector/plonecore/dx/__init__.py
+++ xmldirector/plonecore/dx/__init__.py
@@ -4,3 +4,16 @@
 # xmldirector.plonecore
 # (C) 2014,  Andreas Jung, www.zopyx.com, Tuebingen, Germany
 ################################################################
+
+
+# Workaround for bad Plone 5 behavior :
+# https://github.com/plone/Products.CMFPlone/issues/1533
+
+try:
+    from plone.app.dexterity.browser.types import ALLOWED_FIELDS
+    ALLOWED_FIELDS.append('.xml_field.XML')
+    ALLOWED_FIELDS.append('.xml_image.XML')
+    ALLOWED_FIELDS.append('.xml_binary.XML')
+
+except:
+    pass
