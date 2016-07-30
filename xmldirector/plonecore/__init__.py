29d9b168b2f175e8c21ece703d4989e1cee9fc99
Andreas JungÂ <info@zopyx.com>
2016-07-30 14:31:13 +0200

    updated

--- xmldirector/plonecore/__init__.py
+++ xmldirector/plonecore/__init__.py
@@ -19,7 +19,10 @@ __import__('pkg_resources').declare_namespace(__name__)
 # check minimal versions
 for mod, min_version in [('lxml', [3, 4])]:
     dist = pkg_resources.get_distribution('lxml')
-    dist_version = map(int, dist.parsed_version[:-1])
+    try:
+        dist_version = map(int, dist.parsed_version[:-1])
+    except ValueError:
+        raise ValueError('Unable to parse version {}:{}'.format(mod, dist.parsed_version))
     if not min_version <= dist_version:
         min_version_s = '.'.join(map(str, min_version))
         raise RuntimeError('xmldirector.plonecore requires module {} in version {} or higher (installed: {})'.format(
