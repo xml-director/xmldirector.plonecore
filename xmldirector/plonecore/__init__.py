8d415333233163c79042e03bf0693dc733cf7be7
Andreas JungÂ <info@zopyx.com>
2018-10-29 13:28:14 +0100

    removed version check

--- xmldirector/plonecore/__init__.py
+++ xmldirector/plonecore/__init__.py
@@ -15,19 +15,6 @@ from xmldirector.plonecore.logger import LOG
 
 __import__('pkg_resources').declare_namespace(__name__)
 
-# check minimal versions
-for mod, min_version in [('lxml', [3, 4])]:
-    dist = pkg_resources.get_distribution('lxml')
-    try:
-        dist_version = map(int, dist.parsed_version[:-1])
-    except ValueError:
-        raise ValueError('Unable to parse version {}:{}'.format(
-            mod, dist.parsed_version))
-    if not min_version <= dist_version:
-        min_version_s = '.'.join(map(str, min_version))
-        raise RuntimeError('xmldirector.plonecore requires module {} in version {} or higher (installed: {})'.format(
-            mod, min_version_s, dist.version))
-
 
 # Check filesystem encoding
 fs_enc = sys.getfilesystemencoding()
